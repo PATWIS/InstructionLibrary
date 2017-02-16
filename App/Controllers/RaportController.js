87f563e3ed3aedf89f73c6bf82f440a89b910aea
Patryk Wiśniewski <pwisniewski@infoprojekt.pl>
2017-02-16 10:19:26 +0100

    show loader when filter was changed

--- App/Controllers/RaportController.js
+++ App/Controllers/RaportController.js
@@ -1,94 +1,108 @@
-(function() {
+(function () {
 
     'use strict';
 
-    angular.module('app').controller('RaportController', ['$scope', 'JSOMService', RaportController]);
+    angular.module('app').controller('RaportController', ['$scope', '$timeout', 'dataFiltersFilter', 'JSOMService', 'ngDialog', RaportController]);
 
-    function RaportController($scope, JSOMService) {
+    function RaportController($scope, $timeout, dataFiltersFilter, JSOMService, ngDialog) {
         
-        JSOMService.getSiteUrl().then(function(result) {
+        $scope.sortorder = '3';
+        $scope.quantity = 10;
+        $scope.downloadDataFromSharePointList = true;
+        JSOMService.getSiteUrl().then(function (result) {
             $scope.siteUrl = result;
-
         });
 
-        $scope.finalObject = false;
-        $scope.dataFiltersValues = ["1 maanden", "2 maanden"];
-        $scope.filterValue = $scope.dataFiltersValues[""];
-        $scope.refresh = function() {
-            $scope.finalObject = false;
+        $scope.refresh = function () {
+            $scope.sortorder = '3';
+            $scope.downloadDataFromSharePointList = true;
             init();
         };
 
+        $scope.setLoading = function (loading) {
+            $scope.isLoading = loading;
+        }
 
+        $scope.layoutDone = function (value) {
+            $scope.setLoading(true);
+            $timeout(function () {
+                // take care of the sorting order           
+                $scope.report = dataFiltersFilter($scope.finalObject, value);
+
+                $timeout(function(){
+                    $scope.setLoading(false);
+                }, 0);
+            }, 0);
+        }
 
         // LOCAL STORAGE 
         var storage = JSON.parse(localStorage.getItem("instructions"));
-
+        
         if (storage && storage.length) {
-            $scope.finalObject = false;
             $scope.finalObject = storage;
+            $scope.setLoading(true);
+            $timeout(function() {
+                $scope.report = dataFiltersFilter($scope.finalObject, 3);
+                $timeout(function(){
+                    $scope.downloadDataFromSharePointList = false;
+                    $scope.setLoading(false);
+                }, 0);
+            }, 0);
+
             return;
         }
-
-
 
         init();
 
         function init() {
-
-            
-
-            return JSOMService.getEmployees().then(function(result) {
+            console.log("init");
+            var storage = [];
+            return JSOMService.getEmployees().then(function (result) {
 
                 $scope.employees = result;
 
-            }).then(function() {
+            }).then(function () {
 
-                return JSOMService.getCompetences().then(function(result) {
+                return JSOMService.getCompetences().then(function (result) {
 
-
-                    var resultLookup = result.reduce(function(lookup, competence) {
+                    var resultLookup = result.reduce(function (lookup, competence) {
                         lookup[competence.Id] = competence;
                         return lookup;
                     }, {});
 
-
-                    $scope.employees.forEach(function(employee) {
+                    $scope.employees.forEach(function (employee) {
 
                         $scope.competences = [];
-                        employee.Competences.forEach(function(obj) {
+                        employee.Competences.forEach(function (obj) {
 
                             var competence = resultLookup[obj.Id];
                             if (competence) {
                                 $scope.competences.push(competence);
                             }
-
                         });
                         employee.Competences = $scope.competences;
                     });
-
                 });
-
-            }).then(function() {
+            }).then(function () {
                 // LOAD HERHALING DATA LIST Items --- result 
-                return JSOMService.getHerhalingData().then(function(result) {
+                return JSOMService.getHerhalingData().then(function (result) {
                     $scope.Datas = result;
 
-                    var resultLookupEmployeeInstructionsExist = result.reduce(function(lookup, instructionData) {
+                    var resultLookupEmployeeInstructionsExist = result.reduce(function (lookup, instructionData) {
                         lookup[instructionData.InstructieId + ',' + instructionData.MedewerkerId] = instructionData;
                         return lookup;
                     }, {});
 
-                    var resultLookupMedewerkerId = result.reduce(function(lookup, instructionData) {
+                    var resultLookupMedewerkerId = result.reduce(function (lookup, instructionData) {
                         lookup[instructionData.MedewerkerId] = instructionData;
                         return lookup;
                     }, {});
 
                     $scope.finalObject = [];
 
-                    $scope.employees.forEach(function(employee) {
+                    $scope.employees.forEach(function (employee) {
 
-                        var employeeInstructions = $scope.Datas.filter(function(r) {
+                        var employeeInstructions = $scope.Datas.filter(function (r) {
                             return r.MedewerkerId === employee.Id;
                         })
 
@@ -96,9 +110,9 @@
                             return;
                         }
 
-                        employee.Competences.forEach(function(competence) {
+                        employee.Competences.forEach(function (competence) {
 
-                                competence.Instructions.forEach(function(instruction) {
+                            competence.Instructions.forEach(function (instruction) {
 
                                 var existEmployee = resultLookupMedewerkerId[employee.Id];
 
@@ -112,9 +126,7 @@
                                     return;
                                 }
 
-
-
-                                $scope.inNMonths = function(n) {
+                                $scope.inNMonths = function (n) {
                                     var d = new Date();
                                     d.setMonth(d.getMonth() + n);
                                     return d.toJSON().slice(0, 10);
@@ -126,8 +138,8 @@
                                     return local.toJSON().slice(0, 10);
                                 }
 
-                                $scope.lessThan = function(prop, val) {
-                                    return function(obj) {
+                                $scope.lessThan = function (prop, val) {
+                                    return function (obj) {
                                         var expirty = toJSONLocal(prop);
                                         if (obj[prop] < inNMonths(val)) return true;
                                     }
@@ -141,56 +153,57 @@
                                     CompetenceId: competence.Id,
                                     Competence: competence.Name,
                                     TrainingDate: existInstruction.TrainingDate,
-                                    RepeatDate: existInstruction.RepeatDate
+                                    RepeatDate: existInstruction.RepeatDate,
+                                    uid: employee.Id + '|' + instruction.Id + '|' + competence.Id
                                 });
-
                             })
-
                         })
-
                     });
-                     $scope.finalObject.sort(function (a, b) {
-                            return new Date(b.RepeatDate) - new Date(a.RepeatDate);
-                        });
+                    $scope.finalObject.sort(function (a, b) {
+                        return new Date(b.RepeatDate) - new Date(a.RepeatDate);
+                    });
+
+                    $scope.setLoading(true);
+                    $timeout(function() {
+                        $scope.report = dataFiltersFilter($scope.finalObject, 3);
+                        $timeout(function(){
+                            $scope.setLoading(false);
+                            $scope.downloadDataFromSharePointList = false;
+                        }, 0);
+                    }, 0);
                     localStorage.setItem("instructions", JSON.stringify($scope.finalObject));
+                    
                 });
 
-            }).catch(function(errorMsg) {
+            }).catch(function (errorMsg) {
                 $scope.error = errorMsg;
             });
-
         }
     }
 
-    angular.module('app').filter("dataFilters", function() {
+    angular.module('app').filter("dataFilters", function () {
 
-
-        return function(items, value) {
-
+        return function (items, value) {
 
             if (value && value !== '') {
 
                 var arrayToReturn = [];
                 var d = new Date();
-                d.setMonth(d.getMonth() + parseInt(value) + 1);
+                d.setMonth(d.getMonth() + parseInt(value));
                 // console.log("is number");
 
                 for (var i = 0; i < items.length; i++) {
 
-                    var expiry = new Date(items[i].RepeatDate); 
+                    var expiry = new Date(items[i].RepeatDate);
                     if (expiry < d) {
                         arrayToReturn.push(items[i]);
 
                     }
-
                 }
-
                 return arrayToReturn;
+            } else {
+                return items;
             }
-
-            return items;
-
-
         };
     });
 })();
